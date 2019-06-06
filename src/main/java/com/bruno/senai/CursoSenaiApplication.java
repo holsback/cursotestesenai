969d70ba88ca7669238871b083b4632404b55b0c
holsback <holsback26@gmail.com>
2019-06-06 14:18:10 -0300

    first commit

---
+++ src/main/java/com/bruno/senai/CursoSenaiApplication.java
@@ -0,0 +1,16 @@
+package com.bruno.senai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSenaiApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(CursoSenaiApplication.class);
+	    
+
+	}
+
+}
